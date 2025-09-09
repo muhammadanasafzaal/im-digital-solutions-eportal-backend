c1af01591dbbf8ad3ece541760dbbeaddb86cd52
muhammadanasafzaal <anaskhan.all95@gmail.com>
2025-09-09 18:35:47 +0500

    added test rote

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import app from './app';
 
 const PORT = process.env.PORT || 4000;
+
+app.get('./home', (req, res) => {
+    res.send('test route workin')
+})
+
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
  
